0ce2ea104a90066e046eb417a4203d96f1a43639
Bartosz Rychlicki <bartosz.rychlicki@wenigersh.com>
2013-07-11 13:54:37 +0200

    subscription

---
+++ addLead.php
@@ -0,0 +1,14 @@
+<?php
+if(!empty($_POST['email'])) {
+	$mail = $_POST['email'];
+	// validation should be on front-end side
+	// append e-mail to txt list
+	$file = 'leads.txt';
+	// Open the file to get existing content
+	$current = file_get_contents($file);
+	// Append a new person to the file
+	$current .= "$mail\n";
+	// Write the contents back to the file
+	file_put_contents($file, $current);
+	echo "OK";
+}
