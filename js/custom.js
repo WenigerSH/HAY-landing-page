653f5b26688d28a13ed20064997da444a0770ab1
Paweł Jędrzejczyk <contact@paweljedrzejczyk.com>
2013-07-11 15:46:43 +0200

    WO-6 coded website into html5 without RWD

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	// attach event to add lead form
+	$("form#addLeadForm").on("submit", function(e) {
+		e.preventDefault();
+		$.post($(this).attr("action"), $(this).serialize(), function(data) {
+			if(data == 'OK') {
+				alert('Your e-mail has been saved. Thank you.');
+				$("form#addLeadForm input").val("");
+			}
+		})
+	});
+});
