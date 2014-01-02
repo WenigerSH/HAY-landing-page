00c8197aff9a0125d4f633f3243eb7f65935afa7
Paweł Jędrzejczyk <contact@paweljedrzejczyk.com>
2014-01-03 00:28:31 +0100

    fixed newsletter form

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9c537802a0ac3bb15633fb73e83ee312');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3d30b92e09cc2b56951b9c9d3c51129f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
