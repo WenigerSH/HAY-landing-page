f9c2041924be9975bbc49262a308f2138e8fab63
Paweł Jędrzejczyk <contact@paweljedrzejczyk.com>
2014-01-05 23:34:25 +0100

    90% opacity menu

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a7a9beaf602e49400afff35d5b93c046');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3fbb95b0f79d572aa39e1f02f10b295f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
