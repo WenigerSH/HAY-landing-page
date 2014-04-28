5ed557fe104cf0858ee202b5cf4644508c08ca8f
Paweł Jędrzejczyk <contact@paweljedrzejczyk.com>
2014-04-28 05:01:43 +0200

    added previously removed h3 style

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1772e08efafdf6dc14aa5ade133e9bbd');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1032fd5f30c02355dc49dd2d08669146');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
