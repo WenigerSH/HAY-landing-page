d96906595724cb868ec64f0ea9f89b38f4197a9d
Paweł Jędrzejczyk <contact@paweljedrzejczyk.com>
2014-04-29 17:29:07 +0200

    fixed position of test

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '596bca20a7d7ae7e1194f9b955018ed9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'dbf0ee3edfe9eb411de1bc7558ffac70');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
