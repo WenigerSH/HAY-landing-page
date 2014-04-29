754018d9a4e0c05daeabda7463e5fa80d2df2c89
Paweł Jędrzejczyk <contact@paweljedrzejczyk.com>
2014-04-29 15:42:05 +0200

    resized footer sections

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd4c667717d6ccb6625661f5aed8d6ebe');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '76ba6b79d8e4d63779ef22deb78d2e7c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
