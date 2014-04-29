f14d6b0533fa0d90feb85665da3db4626c54f0e3
Paweł Jędrzejczyk <contact@paweljedrzejczyk.com>
2014-04-29 18:22:49 +0200

    changed color of questions number

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e1b23a576ac097f9ed4c0fb44372e886');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9f4fdbc54b6c8e6c47ecb017ff466d70');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
