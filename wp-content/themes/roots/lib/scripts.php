3714dc68330ea824ce863f22a6ac196c1f4bf44e
Paweł Jędrzejczyk <contact@paweljedrzejczyk.com>
2014-04-29 18:16:38 +0200

    footer fixes

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b770e01d4887803616f6163eeb4541f4');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c3cca56b7c8dd612eaf9116aa01a6be3');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
