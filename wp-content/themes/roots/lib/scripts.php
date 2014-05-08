a73ba4a3e3cf27400f3bdc4554c3afdb716b052b
Paweł Jędrzejczyk <contact@paweljedrzejczyk.com>
2014-05-08 15:53:11 +0200

    changed space between icons

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f26960eea1e316885435bfdcf5ebe95d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7e824eda9ccbb2097c9d015d94851820');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
