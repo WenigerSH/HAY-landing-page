524095173ace3c3f50710b4be5c08f371d518ab4
Paweł Jędrzejczyk <contact@paweljedrzejczyk.com>
2014-05-08 15:50:46 +0200

    added tvp icon

--- wp-content/themes/roots/lib/scripts.php
+++ wp-content/themes/roots/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b36e638d50a594f6ab9bad6e4d2eb35f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f26960eea1e316885435bfdcf5ebe95d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
